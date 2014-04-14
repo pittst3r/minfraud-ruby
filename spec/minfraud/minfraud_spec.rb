af5d6f3700f670631858c305700d6b5758691c38
Robbie Pitts <robbie@sweatypitts.com>
2014-04-14 16:35:46 -0400

    Add configuration validation specs

--- spec/minfraud/minfraud_spec.rb
+++ spec/minfraud/minfraud_spec.rb
@@ -17,6 +17,7 @@ describe Minfraud do
 
   describe '.license_key' do
     let(:license_key) { 'asdf' }
+
     before { Minfraud.remove_class_variable(:@@license_key) if Minfraud.class_variable_defined?(:@@license_key) }
 
     it 'gets license key attribute set on Minfraud module' do
@@ -31,6 +32,7 @@ describe Minfraud do
 
   describe '.license_key=' do
     let(:license_key) { 'asdf' }
+
     before { Minfraud.remove_class_variable(:@@license_key) if Minfraud.class_variable_defined?(:@@license_key) }
 
     it 'sets license key attribute on Minfraud module' do
@@ -42,6 +44,23 @@ describe Minfraud do
   describe '.uri' do
     it 'returns the minFraud service uri' do
       expect(Minfraud.uri).to eq('https://minfraud.maxmind.com/app/ccv2r')
+    end
+  end
+
+  describe '.has_required_configuration?' do
+    let(:license_key) { 'asdf' }
+
+    before do
+      Minfraud.remove_class_variable(:@@license_key) if Minfraud.class_variable_defined?(:@@license_key)
+    end
+
+    it 'returns true if license_key is set' do
+      Minfraud.class_variable_set(:@@license_key, license_key)
+      expect(Minfraud.has_required_configuration?).to be_true
+    end
+
+    it 'returns false if license_key is not set' do
+      expect(Minfraud.has_required_configuration?).to be_false
     end
   end
 
